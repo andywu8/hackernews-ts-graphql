740eee525a4574279d55790cbe52a6db52eced2d
Andy Wu <andyww09@gmail.com>
2023-10-29 19:39:25 -0700

    update resolver for link

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -55,13 +55,22 @@ export const LinkMutation = extendType({  // 1
             },
             
             resolve(parent, args, context) {   
+                const { description, url } = args;
+                const { userId } = context;
+
+                if (!userId) {  // 1
+                    throw new Error("Cannot post without logging in.");
+                }
+
                 const newLink = context.prisma.link.create({
                     data: {
-                        description: args.description, 
-                        url: args.url,
-                    }
-                }) ;
-                return newLink
+                        description,
+                        url,
+                        postedBy: { connect: { id: userId } },  // 2
+                    },
+                });
+
+                return newLink;
             },
         });
     },
